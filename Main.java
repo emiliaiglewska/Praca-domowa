d717ff44c4442d7c8a18d619699aa952933aa789
emiliaiglewska <emilia.iglewska@gmail.com>
2022-08-18 18:47:24 +0200

    Nauka zmiennych z Javie

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej Emilia");
-        System.out.println("cześć");
+        String name = "Emilia";
+        int age = 39;
+        char znak = 'a';
+        int x = 10;
+        int y = 20;
+        int c = x + y;
+       // System.out.println("Hej "+ name + " ktora ma " + age + "lat");
+        System.out.println(c);
+
+
+
     }
 }
